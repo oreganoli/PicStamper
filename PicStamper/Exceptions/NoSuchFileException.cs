2ac3e205e5a9552b9233f05adcfab4c98373f04a
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-16 16:12:57 +0200

    write basic timestamp extractor functions

---
+++ PicStamper/Exceptions/NoSuchFileException.cs
@@ -0,0 +1,13 @@
+namespace PicStamper.Exceptions;
+
+public class NoSuchFileException: Exception
+{
+    public string Path;
+
+    public NoSuchFileException(string path)
+    {
+        Path = path;
+    }
+
+    public override string Message => $"The file {Path} does not exist or is a directory.";
+}
---
+++ PicStamper/TimestampExtractor.cs
@@ -0,0 +1,29 @@
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using PicStamper.Exceptions;
+
+namespace PicStamper;
+
+/// <summary>
+/// Class responsible for getting a timestamp out of a photo.
+/// </summary>
+public static class TimestampExtractor
+{
+    public static string? Timestamp(Stream stream)
+    {
+        var directories = ImageMetadataReader.ReadMetadata(stream);
+        var exif = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
+        var dateTime = exif?.GetDescription(ExifDirectoryBase.TagDateTime);
+        return dateTime;
+    }
+    public static string TimestampFromFile(string path)
+    {
+        if (!File.Exists(path) || (File.GetAttributes(path).HasFlag(FileAttributes.Directory)))
+        {
+            throw new NoSuchFileException(path);
+        }
+        var defaultTime = File.GetCreationTime(path);
+        var imgFile = new FileStream(path, FileMode.Open);
+        return Timestamp(imgFile) ?? defaultTime.ToShortDateString();
+    }
+}
