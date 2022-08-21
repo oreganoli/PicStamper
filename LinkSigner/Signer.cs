44b14d8570b1e4ecf08fd4983fbd2a505f565903
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-21 16:50:30 +0200

    add policy query param

--- LinkSigner/Signer.cs
+++ LinkSigner/Signer.cs
@@ -1,3 +1,4 @@
+using System.Buffers.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
@@ -26,8 +27,17 @@ public static class Signer
         policyString = JsonSerializer.Serialize(JsonSerializer.Deserialize<object>(policyString));
         return policyString;
     }
-    public static string ProduceUrl()
+
+    private static string UrlSafeBase64(string input)
     {
-        throw new NotImplementedException();
+        return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(input))
+            .Replace("+", "-")
+            .Replace("=", "_")
+            .Replace("/", "~");
+    }
+    public static string ProduceUrl(string domain, string prefix, string filename, int secondsValid)
+    {
+        var urlSafePolicy = UrlSafeBase64(MakeUploadPolicy(domain, prefix, secondsValid));
+        return $"https://{domain}/{prefix}/{filename}?Policy={urlSafePolicy}";
     }
 }
