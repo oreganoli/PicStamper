addf074609a590b2806af82bfdc969a435c621d5
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-21 17:16:37 +0200

    remove InputBucket config param

--- LinkSigner/Config.cs
+++ LinkSigner/Config.cs
@@ -3,7 +3,6 @@ namespace LinkSigner;
 public static class Config
 {
     private static string PemKey { get; }
-    private static string InputBucket { get; }
     private static string KeyPairId { get; }
 
     private static string GetEnvVar(string name)
@@ -13,7 +12,6 @@ public static class Config
     static Config()
     {
         PemKey = GetEnvVar("STAMPER_PEM_KEY");
-        InputBucket = GetEnvVar("STAMPER_INPUT_BUCKET");
         KeyPairId = GetEnvVar("STAMPER_KEY_PAIR_ID");
     }
 }
