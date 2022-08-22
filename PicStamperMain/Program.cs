e369a285e1cc86feb32dc22138d0407fae4a32d5
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-22 19:33:03 +0200

    implement saving to S3

--- PicStamperMain/Program.cs
+++ PicStamperMain/Program.cs
@@ -8,12 +8,11 @@ using SharpCompress.Archives.Zip;
 
 // AWS setup.
 var creds = new BasicAWSCredentials(Config.GetEnvVar("AWS_ACCESS_KEY_ID"), Config.GetEnvVar("AWS_SECRET_ACCESS_KEY"));
-var s3Client = new AmazonS3Client(creds, RegionEndpoint.EUCentral1);
 
 // Extract job ID.
 var jobId = args.First();
 // List all files for job ID.
-
+var s3Client = new AmazonS3Client(creds, RegionEndpoint.EUCentral1);
 var files = await s3Client.ListObjectsV2Async(new ListObjectsV2Request
 {
     BucketName = Config.GetEnvVar("INTAKE_BUCKET"),
@@ -26,6 +25,9 @@ var tasks = files.S3Objects.Select(each =>
 {
     var task = Task.Run(async () =>
     {
+        // make s3 client
+        // ReSharper disable once VariableHidesOuterVariable
+        var s3Client = new AmazonS3Client(creds, RegionEndpoint.EUCentral1);
         // Download file.
         var objectResponse = await s3Client.GetObjectAsync(each.BucketName, each.Key);
         var memStream = new MemoryStream();
@@ -45,8 +47,16 @@ var tasks = files.S3Objects.Select(each =>
 var results = await Task.WhenAll(tasks);
 await using var zipFileStream = new FileStream($"{jobId}.zip", FileMode.Create);
 using var zipArchive = ZipArchive.Create();
+using var zipMemStream = new MemoryStream();
 foreach (var (filename, data) in results)
 {
     zipArchive.AddEntry(filename, data);
-}  
-zipArchive.SaveTo(zipFileStream);
+}
+zipArchive.SaveTo(zipMemStream);
+
+await s3Client.PutObjectAsync(new PutObjectRequest
+{
+    BucketName = Config.GetEnvVar("OUTPUT_BUCKET"),
+    Key = $"{jobId}.zip",
+    InputStream = zipMemStream
+});
