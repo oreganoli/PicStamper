ec65b35f23ff988de3bbca2f549471b577d51c16
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-24 16:24:47 +0200

    add job starting in DB

--- PicStamperLambdas/JobStarter/Function.cs
+++ PicStamperLambdas/JobStarter/Function.cs
@@ -1,4 +1,6 @@
 using System.Text.Json;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Model;
@@ -8,16 +10,26 @@ namespace JobStarter;
 
 public class Function
 {
-    public async Task<string> Handler(int jobId, ILambdaContext ctx)
+    public async Task<string> Handler(string jobId, ILambdaContext ctx)
     {
         var lambdaClient = new AmazonLambdaClient();
         var request = new InvokeRequest()
         {
-            Payload = JsonSerializer.Serialize(jobId.ToString()),
+            Payload = JsonSerializer.Serialize(jobId),
             FunctionName = "PicStamperMain",
             InvocationType = InvocationType.Event,
         };
+        // Start job.
         await lambdaClient.InvokeAsync(request);
+        // Update job in DB.
+        var dbClient = new AmazonDynamoDBClient();
+        await dbClient.UpdateItemAsync("PicStamperJobTable", new Dictionary<string, AttributeValue>()
+        {
+            { "jobId", new AttributeValue { S = jobId } }
+        }, new Dictionary<string, AttributeValueUpdate>
+        {
+            { "status", new AttributeValueUpdate { Action = AttributeAction.PUT, Value = new AttributeValue {S = "inProgress"}}}
+        });
         return $"Job number {jobId} accepted for processing.";
     }
 }
