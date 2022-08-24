366d7d193450884232506eb80dcdaabd24bdc6a4
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-24 20:22:54 +0200

    use projection expression instead of attr list

--- PicStamperLambdas/JobLister/Function.cs
+++ PicStamperLambdas/JobLister/Function.cs
@@ -1,4 +1,6 @@
 using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
 // ReSharper disable once CheckNamespace
 namespace JobLister;
 
@@ -7,8 +9,9 @@ public class Function
     public async Task<List<JobModel>> Handler()
     {
         var dbClient = new AmazonDynamoDBClient();
-        var jobs = await dbClient.ScanAsync("PicStamperJobTable",
-            new List<string> { "jobId", "createdAt", "status", "downloadLink" });
+        var scanReq = new ScanRequest
+            { TableName = "PicStamperJobTable", ProjectionExpression = "jobId, createdAt, status, downloadLink"};
+        var jobs = await dbClient.ScanAsync(scanReq);
         return jobs.Items.Select(item => new JobModel
         {
             JobId = item["jobId"].S,
