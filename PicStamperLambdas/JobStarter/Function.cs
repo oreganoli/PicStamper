235b6f230280c5fd64716d54674636fbc88b809a
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-24 15:04:21 +0200

    write a JobStarter function

---
+++ PicStamperLambdas/JobStarter/Function.cs
@@ -0,0 +1,23 @@
+using System.Text.Json;
+using Amazon.Lambda;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Model;
+
+// ReSharper disable once CheckNamespace
+namespace JobStarter;
+
+public class Function
+{
+    public async Task<string> Handler(string jobId, ILambdaContext ctx)
+    {
+        var lambdaClient = new AmazonLambdaClient();
+        var request = new InvokeRequest()
+        {
+            Payload = JsonSerializer.Serialize(jobId),
+            FunctionName = "PicStamperMain",
+            InvocationType = InvocationType.Event,
+        };
+        await lambdaClient.InvokeAsync(request);
+        return $"Job number {jobId} accepted for processing.";
+    }
+}
