5d3dbb087c4b9621c8e0bf014e7955c1c9ede782
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-26 16:50:23 +0200

    remove JobList

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,5 +1,3 @@
 import * as React from "react";
 import * as ReactDom from "react-dom/client";
-import { JobList } from "./components/JobList";
 let root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<JobList />);
