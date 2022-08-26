9decf19e6ddfb2f9dafc3b3fed25e5bbc9bda935
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-26 20:20:19 +0200

    kinda bad code, will correct later

--- frontend/components/UploadForm.jsx
+++ frontend/components/UploadForm.jsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 import { useState } from "react";
+
 export default function UploadForm({ jobId, uploadUrl }) {
     let [canStart, setCanStart] = useState(true);
     let [jobState, setJobState] = useState("pending");
     let [files, setFiles] = useState([]);
-    let stateDescriptions = {
-        "pending": "Start job",
-        "inProgress": "Processing..."
-    };
+    let [uploadNum, setUploadNum] = useState(0);
     let putFiles = (newFiles) => {
         let newFilesArr = [];
         // make a FileList into an array of Files
@@ -23,25 +21,44 @@ export default function UploadForm({ jobId, uploadUrl }) {
     }
     return <>
         <h1>Job #{jobId}</h1>
-        {files.map((file, i, _arr) => <FileEntry filename={file.name} index={i} key={i} removeFile={removeFile} />)}
-        <FilePicker initialValue="" putFiles={putFiles} index={files.length} />
-        <button disabled={!canStart || files.length < 1} onClick={() => {
+        {files.map((file, i, _arr) => <FileEntry canRemove={jobState == "pending"} filename={file.name} index={i} key={i} removeFile={removeFile} />)}
+        <FilePicker canUse={jobState == "pending"} initialValue="" putFiles={putFiles} index={files.length} />
+        <button disabled={!canStart || files.length < 1} onClick={async () => {
             setCanStart(false);
-            setJobState("inProgress");
-        }}>{stateDescriptions[jobState]}</button>
+            await upload("", files, setUploadNum, setJobState);
+        }}>{uploadButtonDescription(jobState, uploadNum, files.length)}</button>
     </>
 }
 
-function FileEntry({ filename, removeFile, index }) {
-    return <div>{filename} <button onClick={() => removeFile(index)}>Remove</button></div>
+function FileEntry({ filename, removeFile, index, canRemove }) {
+    return <div>{filename} {canRemove ? <button onClick={() => removeFile(index)}>Remove</button> : null}</div>;
 }
 
-function FilePicker({ putFiles }) {
+function FilePicker({ canUse, putFiles }) {
     return <div>
         <input accept=".jpg,.jpeg" multiple id="addFilesInput" style={{ "display": "none" }} value="" type="file" onChange={e => {
             e.preventDefault();
             putFiles(e.target.files);
         }}></input>
-        <button onClick={() => document.getElementById("addFilesInput").click()}>Add files</button>
+        <button disabled={!canUse} onClick={() => document.getElementById("addFilesInput").click()}>Add files</button>
     </div >
+}
+
+async function upload(_uploadLink, files, setUploadNum, setJobState) {
+    for (let i = 0; i < files.length; i++) {
+        console.log(`Uploading file #${i}, ${files[i].name}...`)
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        setUploadNum(i + 1);
+    }
+    setJobState("processing");
+}
+
+function uploadButtonDescription(state, number, maxNumber) {
+    if (state === "pending") {
+        return "Start job";
+    } else if (state === "inProgress") {
+        return `Please wait, uploading file ${number}/${maxNumber}...`;
+    } else if (state == "processing") {
+        return "Please wait, processing...";
+    }
 }
