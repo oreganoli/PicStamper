105dfff638373468198585ee960aa29ff0d8e5ea
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-26 21:38:34 +0200

    fixed upload screen

--- frontend/components/UploadScreen.jsx
+++ frontend/components/UploadScreen.jsx
@@ -4,14 +4,17 @@ import { useState, useEffect } from "react";
 export default function UploadScreen({ jobId, files }) {
     let [currentFileNo, setCurrentFileNo] = useState(0);
     useEffect(() => {
-        if (currentFileNo == files.length - 1) {
-            return;
-        }
-        console.log(`Uploading file #${currentFileNo}, ${files[currentFileNo].name}...`)
-        new Promise(resolve => setTimeout(resolve, 2000)).then(() => setCurrentFileNo(x => x + 1));
-    }, [currentFileNo]);
+        (async () => {
+            for (let i = 0; i < files.length; i++) {
+                setCurrentFileNo(i);
+                console.log(`Uploading file #${i + 1}/${files.length}, ${files[i].name}...`);
+                await new Promise(resolve => setTimeout(resolve, 2000));
+                console.log(`Uploaded file #${i + 1}/${files.length}, ${files[i].name}.`);
+            }
+        })().then(x => setCurrentFileNo(x => x + 1));
+    }, [files]);
     return <>
         <h1>Job #{jobId}</h1>
-        {(currentFileNo) < files.length ? <p>Please wait, uploading file {currentFileNo + 1}/{files.length}...</p> : <p>Upload complete.</p>}
+        {(currentFileNo) < files.length ? <p>{`Please wait, uploading file ${files[currentFileNo].name}(${currentFileNo + 1}/${files.length})...`}</p> : <p>Upload complete.</p>}
     </>
 }
