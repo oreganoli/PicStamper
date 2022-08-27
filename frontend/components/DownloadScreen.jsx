e19c062752c85e68bed0c281e70ac93463ad7a22
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-08-27 18:45:01 +0200

    Implemented download screen

--- frontend/components/DownloadScreen.jsx
+++ frontend/components/DownloadScreen.jsx
@@ -1,16 +1,24 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
+import Axios from "axios";
 
 export default function DownloadScreen({ jobId }) {
     let [ticker, setTicker] = useState(0);
     let [downloadLink, setDownloadLink] = useState("");
     useEffect(() => {
         (async () => {
-            for (let i = 0; i < 5; i++) {
-                await new Promise(resolve => setTimeout(resolve, 2000));
+            while (true) {
                 setTicker(x => x + 1);
+                let response = await Axios.get(`https://picstamper-api.oreganoli.xyz/jobs/${jobId}`);
+                let jobInfo = response.data;
+                if (jobInfo.DownloadLink !== "") {
+                    setDownloadLink(jobInfo.DownloadLink);
+                    break;
+                } else {
+                    await new Promise(resolve => setTimeout(resolve, 5000));
+                }
             }
-        })().then(() => setDownloadLink("test"));
+        })().then(() => console.log("Processing complete."));
     }, [jobId]);
     if (downloadLink === "") {
         return <>
@@ -21,6 +29,8 @@ export default function DownloadScreen({ jobId }) {
         return <>
             <h1>Success</h1>
             <h2>The job was finished.</h2>
+            <a id="downloadLink" href={downloadLink} download style={{ "display": "none" }}></a>
+            <button onClick={() => document.getElementById("downloadLink").click()}>Download file</button>
         </>
     }
 }
